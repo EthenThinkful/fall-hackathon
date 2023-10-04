4575d5eeb48ded8cbe9125f3c02dc230e3c55d25
Genesee Harmon <geneseelh@gmail.com>
2023-10-03 21:48:54 -0700

    created basic userprofile layout

--- src/UserProfile/UserProfile.jsx
+++ src/UserProfile/UserProfile.jsx
@@ -1,13 +1,71 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import "./UserProfile.css";
+import { useNavigate } from "react-router-dom";
+
+// defines the user profile of the application
 
 function UserProfile() {
+  const user = {
+    username: "JaneD2013",
+    email: "janed2013@gmail.com",
+    phone: "+1 (555)-555-5555",
+  };
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("../course");
+  };
+
   return (
     <>
-    <NavBar />
-    <div>UserProfile</div>
+      <NavBar />
+      <div>
+        <div className="profile-header">
+          <h1>My Profile</h1>
+        </div>
+        <div className="name-section">
+          <div className="name-pfp-container">
+            {/* pfp img */}
+            <img
+              src="profile-picture.png"
+              alt="User Profile Picture"
+              className="profile-picture"
+            />
+            <h2>Jane Doe</h2>
+          </div>
+        </div>
+        {/* user information */}
+        <div className="info-section">
+          <div className="user-info-container">
+            <p>Username</p>
+            <p className="info-field">{user.username}</p>
+            <p>Email</p>
+            <p className="info-field">{user.email}</p>
+            <p>Phone</p>
+            <p className="info-field">{user.phone}</p>
+          </div>
+        </div>
+
+        <div>
+          <h3>Completed Courses</h3>
+          <div className="completed-courses-container">
+            {/* will link to courses page */}
+            <button className="course-box" onClick={handleSubmit}>
+              Click me to go to your courses
+            </button>
+            <button className="course-box" onClick={handleSubmit}>
+              Click me to go to your courses
+            </button>
+            <button className="course-box" onClick={handleSubmit}>
+              Click me to go to your courses
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default UserProfile
+export default UserProfile;
