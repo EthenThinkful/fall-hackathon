65b13af4f701d3f8bd3b31139d4559b4aded2112
James-Ukoli <thejamesukoli@gmail.com>
2023-10-05 21:14:39 -0500

    Search template roudh draft

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -7,8 +7,16 @@ import { useNavigate } from 'react-router-dom';
 
 
 function Search({searchResults, setSearchResults, toBeSearched, setToBeSearched}) {
+
   const [isOpen, setIsOpen] = useState(false);
   console.log(searchResults.length);
+
+  var firstCharacterInSearch = searchResults.charAt(0);
+  var firstCharacterInSearchUpper = firstCharacterInSearch.toUpperCase();
+
+  var restOfCharactersSearch = searchResults.slice(1);
+
+
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
@@ -30,10 +38,57 @@ const handleSearch = () => {
   return (
     <>
     {/* <NavBar searchResults={searchResults} setSearchResults={setSearchResults}/><button onClick={handleSearch}>Search</button> */}
-    {toBeSearched.length === 0 ? 
+    {toBeSearched.length > 0 ? 
     <div>
-      <h1>Search Page</h1>
-        <div>{searchResults}</div>
+     
+  <br></br>
+  <br></br>
+<div className='result-data-container'>
+      <h2>{firstCharacterInSearchUpper}{restOfCharactersSearch}</h2>
+</div>
+      <div className='result-data-container'>
+        <p>456 results</p>
+      </div>
+        <br></br>
+        <br></br>
+        <div className='result-data-container filter-button'>Filter</div>
+        <br></br>
+        <br></br>
+        <div className='result-data-container'> 
+          <h3>{searchResults} for Beginners</h3>
+        </div>
+        <div className='results-cards'>
+          <div className="course-container">
+            <button className='course-box'>HI</button>
+          </div>
+          <div className="course-container">
+            <button className='course-box'>HI</button>
+          </div>
+          <div className="course-container">
+            <button className='course-box'>HI</button>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className='result-data-container'>
+          <h3>{searchResults} for Intermediates</h3>
+        </div>
+        <div className='results-cards'>
+          <div className="course-container">
+            <button className='course-box'>HI</button>
+          </div>
+          <div className="course-container">
+            <button className='course-box'>HI</button>
+          </div>
+          <div className="course-container">
+            <button className='course-box'>HI</button>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+      <div className='result-data-container'>
+          <h3>More results for {searchResults}..</h3>
+        </div>
           <div className="course-container">
           <button className='course-box' onClick={handleSubmit}>
           Click here to take yourself to the page for this course
