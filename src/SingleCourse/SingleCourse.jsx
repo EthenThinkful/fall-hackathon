2472cf03c97d28b824145031331b5e257ae75eb1
Odonchimeg Myagmar <om12odnoo@gmail.com>
2023-10-06 15:15:53 -0500

    some changes in single course page

--- src/SingleCourse/SingleCourse.jsx
+++ src/SingleCourse/SingleCourse.jsx
@@ -1,36 +1,59 @@
 import React from 'react';
 import './SingleCourse.css';
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
+import checkMark from '../Images/check-mark.png'
 
 function SingleCourse() {
   return (
     <>
         <div className='all-content'>
+            {/* begin top section */}
             <div className='top-section'>
                 <div className='top-content'>
-                    <img src='../Images/arrow2.jpg' />
-                    <h1>Traditional Italian Cooking</h1>
-                    <p>Jamie's Kitchen</p>
-                    <img src={require('../Images/kitchen.png')} />
-                    <div className='row'>
-                        <div className='text'>
-                            <p>24,000 Likes</p>
+                    <div>
+                    <img src={require('../Images/arrow.jpg')} style={{marginBottom: '1rem'}}/>
+                        <div style={{display: 'flex', flexDirection: 'column'}}>
+                        <div className='cooking-title'>Traditional Italian Cooking</div>
+                        <div className='author-title'>Jamie's Kitchen</div>
+                        <img src={require('../Images/kitchen.png')} />
+                        {/* begin like share section */}
+                        <div className='row'>
+                            <div className='text'>
+                            24,000 Likes
+                            </div>
+                            <div className='buttons'>
+                                <button id='LikeButton'>Like</button>
+                                <button id='ShareButton'>Share</button>
+                            </div>
                         </div>
-                        <div className='buttons'>
-                            <button id='LikeButton'>Like</button>
-                            <button id='ShareButton'>Share</button>
-                        </div>
+                        <div className='navbar'>
+                        <ul>
+                            <li><a href='/about'></a><span class='underline'>About</span></li>
+                            <li><a href='/reviews'></a>Reviews</li>
+                            <li><a href='discussion-board'></a>Discussion Board</li>
+                            <li><a href='resources'></a>Resources</li>
+                            <li><a href='q-and-a'></a>Q&A</li>
+                        </ul>
                     </div>
-                </div>
-                <div className='scroll-container'>
+                    {/* end like share section */}
+                    </div>
+                    </div>
+                    {/* begin scroll container */}
+                    
+                    <div className='scroll-container'>
                     <div class='content'>
                         <h4>Assignment due October 27</h4>
                         <p>15 Lessons (1 hr 28 min)</p>
                             <ol>
-                            <img src='../Image/check-mark.png' />
-                                <li><span class='checked'>Italian Spices</span></li>
-                                <li>Pantry Must Haves</li>
-                                <li>Cooking Utensils</li>
-                                <li>Noodles</li>
+                                <div style={{display: 'flex'}}>
+                            <span className="checkmark"></span><li>
+                                    Italian Spices
+                                </li>
+                                </div>
+                               <li className='li'><span className="checkmark"></span>Pantry Must Haves</li>
+                                <li><span className="checkmark"></span>Cooking Utensils</li>
+                                <li><span className="checkmark"></span>Noodles</li>
                                 <li>Pasta Sauce</li>
                                 <li>Choosing the Best Produce</li>
                                 <li>Cooking Temperatures</li>
@@ -40,44 +63,65 @@ function SingleCourse() {
                             </ol>
                         </div> 
                     </div>
+                    
+                {/* end scroll container */}                  
                 </div>
+            </div>
             {/* end of top section */}
 
+            {/* start bottom section */}
             <div className='bottom-section'>
+                {/* startleft container */}
                 <div className='left-container'>
-                    <div className='navbar'>
-                        <ul>
-                            <li><a href='/about'></a><span class='underline'>About</span></li>
-                            <li><a href='/reviews'></a>Reviews</li>
-                            <li><a href='discussion-board'></a>Discussion Board</li>
-                            <li><a href='resources'></a>Resources</li>
-                            <li><a href='q-and-a'></a>Q&A</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h1>About this Class</h1>
-                        <p>The course on traditional Italian cooking offers a comprehensive exploration of Italy's rich culinary heritage. Participants will delve into the fundamental principles that underpin this renowned cuisine, including an emphasis on fresh, high-quality ingredients, and the art of balancing flavors to create delectable dishes.</p>
-                        <p>From mastering the art of pasta-making to understanding the nuances of regional sauces and incorporating essential Mediterranean ingredients, this course provides a hands-on and in-depth look at the techniques and traditions that have made Italian cooking a global culinary sensation. Through practical demonstrations and interactive cooking sessions, participants will gain a deep appreciation for the time-honored methods that define traditional Italian gastronomy.</p>
+                    <div style={{display: 'flex', flexDirection: 'column', marginLeft: '3rem' }}>
+                        <div style={{ textAlign: 'left', fontSize: '1.5rem', fontWeight: 'bold' }}>About this Class</div>
+                        <div style={{textAlign: 'left', marginTop: '2rem'}}>The course on traditional Italian cooking offers a comprehensive exploration of Italy's rich culinary heritage. Participants will delve into the fundamental principles that underpin this renowned cuisine, including an emphasis on fresh, high-quality ingredients, and the art of balancing flavors to create delectable dishes.</div>
+                        <div style={{textAlign: 'left', marginTop: '2rem'}}>From mastering the art of pasta-making to understanding the nuances of regional sauces and incorporating essential Mediterranean ingredients, this course provides a hands-on and in-depth look at the techniques and traditions that have made Italian cooking a global culinary sensation. Through practical demonstrations and interactive cooking sessions, participants will gain a deep appreciation for the time-honored methods that define traditional Italian gastronomy.</div>
                     </div>
                 </div>
+                {/* end left container */}
+
+                {/* start right container */}
                 <div className='right-container'>
-                    <h1>Teacher</h1>
-                    <div className='image-title'>
-                        <div className='image'>
-                            <img src={require("../Images/photo.jpg")} />
+                    {/* start flex column */}
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        {/* 1.) */}
+                        {/* start teacher */}
+                        <div style={{ display: 'flex', alignItems: 'flex-start', marginBottom: '1rem'}}>
+                            <div>Teacher</div>
                         </div>
-                        <div className='title'>
-                            <p>Jamie's Kitchen</p>
-                            <p>45K Likes</p>
-                    </div>
-                        <div className='follow-button'>
+                        {/* end teacher */}
+
+                        {/* 2.) */}
+                        {/* start teacher profile picture and course title / likes */}
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <div className='image'>
+                                    <img src={require("../Images/photo.png")} />
+                                </div>
+                                <div className='title'>
+                                    <p>Jamie's Kitchen</p>
+                                    <p>45K Likes</p>
+                            </div>
+                        </div>
+                        {/* end teacher profile picture and course title / likes */}
+
+                        {/* 3.) */}
+                        {/* start button */}
+                        <div style={{ display: 'flex', alignItems: 'flex-start', marginBottom: '1rem' }}>
                             <button id='FollowButton'>Follow</button>
                         </div>
-                        <div>
-                            <p>Chef Jamie's Kitchen, curated by the visionary Chef Jamie, is a culinary sanctuary in the heart of the city. With a passion </p>
+                        {/* end button */}
+
+                        {/* 4.) */}
+                        {/* start description */}
+                        <div style={{ display: 'flex', alignItems: 'flex-start', flexWrap: 'wrap'}}>
+                            <div style={{ textAlign: 'left'}}>Chef Jaime's Kitchen, curated by the visionary Chef Jamie, is a culinary sanctuary in the heart of the city. With a passion for flavors and a dedication to using the finest ingredients, Chef Jamie crafts exceptional dishes that leave a lasting impression. </div>
                         </div>
+                        {/* end description */}
                     </div>
+                    {/* end flex column */}
                 </div>
+                {/* end right container */}
             </div>
             {/* end of bottom section */}
         </div>
