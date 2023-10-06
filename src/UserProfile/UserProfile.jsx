dfc93bac61403ed9b29ff0732a81002dc04e6c78
Genesee Harmon <geneseelh@gmail.com>
2023-10-05 19:09:00 -0700

    updated user profile styling

--- src/UserProfile/UserProfile.jsx
+++ src/UserProfile/UserProfile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar";
 import "./UserProfile.css";
 import { useNavigate } from "react-router-dom";
 import userPfp from "../Image/user-pfp.png";
+import italianPic from "../Image/italian-for-travelers.jpg";
+import dots from "../Image/3-buttons.png";
 
 // defines the user profile of the application
 
@@ -25,48 +26,74 @@ function UserProfile() {
         <div className="profile-header">
           <h1>My Profile</h1>
         </div>
-        <div className="name-section">
-          {/* <img src={userHeader} alt="User Header Image" className="name-pfp-container"/> */}
-          <div className="name-pfp-container">
-            {/* pfp img */}
-            <img
-              src={userPfp}
-              alt="User Profile Picture"
-              className="profile-picture"
-            />
-            <h2>Jane Doe</h2>
+        {/* <img src={userHeader} alt="User Header Image" className="name-pfp-container"/> */}
+
+        <div className="name-pfp-container">
+          <div className="profile-container">
+            <div className="edit-profile">
+              <img
+                src={userPfp}
+                alt="User Profile Picture"
+                className="profile-picture"
+              />
+              <h2>Jane Doe</h2>
+              <button className="edit-profile-btn">Edit Profile</button>
+            </div>
+            {/* <h2>Jane Doe</h2> */}
           </div>
         </div>
+        <h3>Completed Courses</h3>
+
         {/* user information */}
         <div className="info-section">
           <div className="user-info-container">
-            <p>Username</p>
+            <p style={{ fontWeight: "700", margin: "4px 0" }}>Username</p>
             <p className="info-field">{user.username}</p>
-            <p>Email</p>
+            <p style={{ fontWeight: "700", margin: "0" }}>Email</p>
             <p className="info-field">{user.email}</p>
-            <p>Phone</p>
+            <p style={{ fontWeight: "700", margin: "0" }}>Phone</p>
             <p className="info-field">{user.phone}</p>
           </div>
-        </div>
-
-        <div>
-          <h3>Completed Courses</h3>
           <div className="completed-courses-container">
             {/* will link to courses page */}
             {/* style this */}
-            <div className="course-img-1">
-              <button className="course-box" onClick={handleSubmit}></button>
-              <p>Painting with Acrylic - 10 Lessons</p>
+            <div className="course-img">
+              <img className="course-box-two" src={italianPic}></img>
+              <div className="testPTag">
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  Painting with Acrylic - 10 Lessons
+                  <img src={dots}></img>
+                </div>
+                <div style={{display: "flex"}}>Maisy's Studio</div>
+              </div>
             </div>
             {/* style this */}
-            <div className="course-img-2">
-              <button className="course-box" onClick={handleSubmit}></button>
-              <p>Italian Appetizers - 12 Lesson</p>
+            <div className="course-img">
+              <img className="course-box-two" src={italianPic}></img>
+              <div className="testPTag">
+              <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  Painting with Acrylic - 10 Lessons
+                  <img src={dots}></img>
+                </div>
+                <div style={{display: "flex"}}>Maisy's Studio</div>
+              </div>
             </div>
             {/* style this */}
-            <div className="course-img-3">
-              <button className="course-box" onClick={handleSubmit}></button>
-              <p>Watercolor Patterns - 6 Lessons</p>
+            <div className="course-img">
+              <img className="course-box-two" src={italianPic}></img>
+              <div className="testPTag">
+              <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  Painting with Acrylic - 10 Lessons
+                  <img src={dots}></img>
+                </div>
+                <div style={{display: "flex"}}>Maisy's Studio</div>
+              </div>
             </div>
           </div>
         </div>
