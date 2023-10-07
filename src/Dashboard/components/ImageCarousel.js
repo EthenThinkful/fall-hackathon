b01407c777b4dc45bbe8dfe5177d8b122228404d
EthenThinkful <beatsby3t@gmail.com>
2023-10-06 21:39:42 -0600

    finished carousel for all pages

--- src/Dashboard/components/ImageCarousel.js
+++ src/Dashboard/components/ImageCarousel.js
@@ -21,8 +21,8 @@ function ImageCarousel() {
           <img src={course.image} alt={`Image ${index + 1}`} className="carousel-image" />
           {/* Ethen adding code */}
           <div className="carousel-desc-duo">
-          <p className="carousel-title"><strong>{course.title}</strong></p>
-          <p className="carousel-instructor"><strong>{course.instructor}</strong></p>
+          <p className="carousel-title"><strong>{course.title}</strong></p><img src={dots} alt="dots" className="dot-menuu"/>
+          <p className="carousel-instructor" style={{fontWeight: "700"}}><strong>{course.instructor}</strong></p>
           </div>
           {/* Ethen end adding code */}
         </div>
@@ -39,6 +39,48 @@ function ImageCarousel() {
       autoPlay={false}
       centerMode={true}
       centerSlidePercentage={50}
+      renderArrowPrev={(onClickHandler, hasPrev, label) =>
+        hasPrev && (
+          <button
+            type="button"
+            onClick={onClickHandler}
+            title={label}
+            className="arrow-left"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              id="angle-left-b"
+            >
+              <path
+                fill="#8F8F8F"
+                d="m8.5 12.8 5.7 5.6c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4l-4.9-5 4.9-5c.4-.4.4-1 0-1.4-.2-.2-.4-.3-.7-.3-.3 0-.5.1-.7.3l-5.7 5.6c-.4.5-.4 1.1 0 1.6 0-.1 0-.1 0 0z"
+              ></path>
+            </svg>
+          </button>
+        )
+      }
+      renderArrowNext={(onClickHandler, hasNext, label) =>
+        hasNext && (
+          <button
+            type="button"
+            onClick={onClickHandler}
+            title={label}
+            className="arrow-right"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              id="angle-right-b"
+            >
+              <path
+                fill="#8F8F8F"
+                d="M15.54,11.29,9.88,5.64a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41l4.95,5L8.46,17a1,1,0,0,0,0,1.41,1,1,0,0,0,.71.3,1,1,0,0,0,.71-.3l5.66-5.65A1,1,0,0,0,15.54,11.29Z"
+              ></path>
+            </svg>
+          </button>
+        )
+      }
     >
       {renderSlides}
     </Carousel>
--- src/Dashboard/components/data.js
+++ src/Dashboard/components/data.js
@@ -91,7 +91,7 @@ export const courses = [
         image: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
       },
       {
-        title: "An Introduction to Darkroom Photography",
+        title: "Intro to Darkroom Photography",
         category: "photography",
         level: "intermediate",
         lessons: 21,
